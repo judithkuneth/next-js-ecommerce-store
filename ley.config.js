10f14188e57236a54b50455ef676a8e890bbb63c
Judith Kuneth <judith.kuneth@gmail.com>
2020-10-19 16:51:10 +0200

    Add ley and ley-cli to setup migrations

--- ley.config.js
+++ ley.config.js
@@ -1,8 +1,12 @@
 //this file was created during the process of deploying this app to heroku. See Configure you Application Step 6: "Create a configuration file for Ley called ley.config.js.." DONE
 
+const extractHerokuDatabaseEnvVars = require('./extractHerokuDatabaseEnvVars');
+
 //..which imports and runs the function from step 2". TO DO
 
 //Step 7: Configure SSL database connections in ley.config.js:
+
+extractHerokuDatabaseEnvVars();
 const options = {};
 
 if (process.env.NODE_ENV === 'production') {
